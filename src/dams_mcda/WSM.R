a0b3cef6d613f367eb509ad57cbcd75820b73407
William Winslow <thesythel@gmail.com>
2018-11-26 16:39:35 -0500

    changed output of WSM's plot * now returns data instead of barplot so barplot can be rendered in server.R
    * more/fixed debug prints
    
    * waiting on input on issue #6 for desired output plot
    
    * remade the min/max normilization so future changes in function will require less time and its now easier to debug

--- src/dams_mcda/WSM.R
+++ src/dams_mcda/WSM.R
@@ -37,7 +37,9 @@ library(dplyr)
 
 colors <- c("darkblue", "purple", "green", "red", "yellow", "orange", "pink")
 
-#Garrett's version of MCDA fn, modified for DAM DECISION purposes
+#--------------------------------------------------------------------------------
+# Garrett's version of MCDA fn, modified for DAM DECISION purposes
+#--------------------------------------------------------------------------------
 WSM <- function(CritImportance, RawCriteriaMatrix){
 	message('--------------------------------------------------------------------------------')
 	message('WSM init')
@@ -50,57 +52,56 @@ WSM <- function(CritImportance, RawCriteriaMatrix){
 	#Fill min/max vectors
 	#for (k in 1:7){
 	WSMMaxVector <- c(max(RawCriteriaMatrix[,1], na.rm = FALSE), max(RawCriteriaMatrix[,2], na.rm = FALSE), max(RawCriteriaMatrix[,3], na.rm = FALSE),
-					   max(RawCriteriaMatrix[,4], na.rm = FALSE), max(RawCriteriaMatrix[,5], na.rm = FALSE), max(RawCriteriaMatrix[,6], na.rm = FALSE), 
+					   max(RawCriteriaMatrix[,4], na.rm = FALSE), max(RawCriteriaMatrix[,5], na.rm = FALSE), max(RawCriteriaMatrix[,6], na.rm = FALSE),
 					   max(RawCriteriaMatrix[,7], na.rm = FALSE))
 
-	WSMMinVector <- c(min(RawCriteriaMatrix[,1], na.rm = FALSE), min(RawCriteriaMatrix[,2], na.rm = FALSE), min(RawCriteriaMatrix[,3], na.rm = FALSE), 
-					  min(RawCriteriaMatrix[,4], na.rm = FALSE), min(RawCriteriaMatrix[,5], na.rm = FALSE), min(RawCriteriaMatrix[,6], na.rm = FALSE), 
+	WSMMinVector <- c(min(RawCriteriaMatrix[,1], na.rm = FALSE), min(RawCriteriaMatrix[,2], na.rm = FALSE), min(RawCriteriaMatrix[,3], na.rm = FALSE),
+					  min(RawCriteriaMatrix[,4], na.rm = FALSE), min(RawCriteriaMatrix[,5], na.rm = FALSE), min(RawCriteriaMatrix[,6], na.rm = FALSE),
 					  min(RawCriteriaMatrix[,7], na.rm = FALSE))
 
-	message('min vector')
-	message(WSMMinVector)
-	message('max vector')
-	message(WSMMaxVector)
+	message('min vector', WSMMinVector)
+	message('max vector', WSMMaxVector)
+	message('critical matrix', CritImportance)
 
 	#Build Score Matrix
 	WSMScoreMatrix <- data.frame(matrix(data=NA, nrow = 6, ncol = 7))
 
 	message('WSM score matrix start')
+
+	# array of rows that used alternative method
+	alt_method_rows <- c(4, 6)
+
+	# make normalized values of each value in matrix
 	for (k in 1:7){
 		for (n in 1:6){
-			if (k == 1){
-				WSMScoreMatrix[n,k] <- ((RawCriteriaMatrix[n,k]-WSMMinVector[k])/(WSMMaxVector[k]-WSMMinVector[k]))*CritImportance[k]
-			}else if (k == 2){
-				WSMScoreMatrix[n,k] <- ((RawCriteriaMatrix[n,k]-WSMMinVector[k])/(WSMMaxVector[k]-WSMMinVector[k]))*CritImportance[k]
-			}else if (k == 3){
-				WSMScoreMatrix[n,k] <- ((RawCriteriaMatrix[n,k]-WSMMinVector[k])/(WSMMaxVector[k]-WSMMinVector[k]))*CritImportance[k]
-			}else if (k == 4){
-				WSMScoreMatrix[n,k] <- ((WSMMaxVector[k]-RawCriteriaMatrix[n,k])/(WSMMaxVector[k]-WSMMinVector[k]))*CritImportance[k]
-			}else if (k == 5){
-				WSMScoreMatrix[n,k] <- ((RawCriteriaMatrix[n,k]-WSMMinVector[k])/(WSMMaxVector[k]-WSMMinVector[k]))*CritImportance[k]
-			}else if (k == 6){
-				WSMScoreMatrix[n,k] <- ((WSMMaxVector[k]-RawCriteriaMatrix[n,k])/(WSMMaxVector[k]-WSMMinVector[k]))*CritImportance[k]
-			}else if (k == 7){
-				WSMScoreMatrix[n,k] <- ((RawCriteriaMatrix[n,k]-WSMMinVector[k])/(WSMMaxVector[k]-WSMMinVector[k]))*CritImportance[k]
-			}
-			message('raw value')
-			message(RawCriteriaMatrix[n,k])
-			message('new value')
-			message(WSMScoreMatrix[n,k])
+			x <- RawCriteriaMatrix[n,k]
+			min_x <- WSMMinVector[k]
+			max_x <- WSMMaxVector[k]
+			crit_imp <- CritImportance[k]
+
+			WSMScoreMatrix[n,k] <- tryCatch({
+				if (k %in% alt_method_rows){
+					# alternative? method
+					(((max_x - x) / (max_x - min_x)) * crit_imp)
+				}else{
+					# default min/max normilization
+					(((x - min_x) / (max_x - min_x)) * crit_imp)
+				}
+			}, error=function(e){
+				return(NA)
+			})
 
 		} #End alternative (rows) for loop.
 	} #End criteria (columns) for loop.
-	message('WSMScoreMatrix')
-	message(WSMScoreMatrix)
+
+	message('WSMScoreMatrix', WSMScoreMatrix)
 
 	IntermediateMatrix <- data.frame(matrix(data=NA, nrow=6, ncol=7))
 	IntermediateMatrix[1:6, 1:7] <- round(WSMScoreMatrix,3)
 
-	message('IntermediateMatrix')
-	message(IntermediateMatrix)
+	message('IntermediateMatrix', IntermediateMatrix)
 
-	message('WSM score sum')
-	#for (n in 1:6){
+	# total score is last column of returned Data Table
 	scoresum <- c(NA, NA, NA, NA, NA, NA)
 	scoresum <- rbind(c(sum(as.numeric(IntermediateMatrix[1:6,1]))),
 					  c(sum(as.numeric(IntermediateMatrix[1:6,2]))),
@@ -108,31 +109,19 @@ WSM <- function(CritImportance, RawCriteriaMatrix){
 					  c(sum(as.numeric(IntermediateMatrix[1:6,4]))),
 					  c(sum(as.numeric(IntermediateMatrix[1:6,5]))),
 					  c(sum(as.numeric(IntermediateMatrix[1:6,6]))))
-	#}
-	message('WSM score sum done')
+	message('WSM score sum done', scoresum)
 
-	WSMScoreDF <- as.matrix(data.frame(cbind(IntermediateMatrix, scoresum)))
+	WSMScoreDF <- data.frame(cbind(IntermediateMatrix, scoresum))
 	t_IntermediateMatrix <- t(IntermediateMatrix)
 
-	row.names(WSMScoreDF) <- paste(c("Dam Removal", "Fish Improve", "Turbine Improve", "Turbine Add or Expand", "Dam Refurbish or Maintain", "Keep Dam"), sep = " ")
-	colnames(WSMScoreDF) <- paste(c("Fish Biomass", "River Recreation", "Reservoir Storage", "One-Time Project Costs", "Number of Properties Impacted", "Dam Safety", "Hydropower Capacity", "Summed Score"), sep = " ")
-	message('WSM names done')
+	# column/row names of WSMTable
+	row.names(WSMScoreDF) <- c("Dam Removal", "Fish Improve", "Turbine Improve", "Turbine Add or Expand", "Dam Refurbish or Maintain", "Keep Dam")
+	colnames(WSMScoreDF) <- c("Fish Biomass", "River Recreation", "Reservoir Storage", "One-Time Project Costs", "Number of Properties Impacted", "Dam Safety", "Hydropower Capacity", "Summed Score")
 
-	#Make Bar Chart
-	WSMBar <- barplot(t_IntermediateMatrix, main="WSM Ranked Alternatives",
-					  xlab=c("Dam Removal", "Fish Improve", "Turbine Improve", "Turbine Add or Expand", "Dam Refurbish or Maintain", "Keep Dam"),
-					  col=colors,
-					  legend = c("Fish Biomass", "River Recreation", "Reservoir Storage", "One-Time Costs", "Safety", "Number of Houses", "Hydropower Capacity"))
+	# Assign data to be used for BarPlot
+	WSMBarPlotData <- t_IntermediateMatrix
 
-	WSMResults <- list(WSMScoreDF, WSMBar, IntermediateMatrix)
-	message('WSM results done')
+	WSMResults <- list(WSMScoreDF, WSMBarPlotData)
+
 	return(WSMResults)
 }
-
-####SAVE THIS EXAMPLE CODE TO TROUBLESHOOT PLOT DESIGN LATER########
-
-#WSMResults <- WSM(CritImportance=CritImportance, RawCriteriaMatrix=RawCriteriaMatrix)
-#WSMResults[1]
-#WSMResults[2]
-
-####SAVE THIS EXAMPLE CODE TO TROUBLESHOOT PLOT DESIGN LATER########
