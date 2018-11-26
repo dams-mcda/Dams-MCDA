a8ebda36f622a9cc271d1ab6564a96a16bcc79f9
William Winslow <thesythel@gmail.com>
2018-11-26 16:36:03 -0500

    server syntax update, all graphs but output now working

--- src/dams_mcda/server.R
+++ src/dams_mcda/server.R
@@ -34,6 +34,7 @@ colors <- c("darkblue", "purple", "green", "red", "yellow", "orange", "pink")
 score_range = c(1, 5)
 # default names of the fields
 variable_names <- c("Fish Biomass", "River Recreation", "Reservoir Storage", "One-Time Project Costs", "Safety", "Number of Properties Impacted", "Hydropower Capacity")
+wsm_bar_plot_names <- c("Dam Removal", "Fish Improve", "Turbine Improve", "Turbine Add or Expand", "Dam Refurbish or Maintain", "Keep Dam")
 
 #----------------------------------------
 # Misc.
@@ -92,8 +93,9 @@ loadData <- function() {
 # xpd == False disables bars being drawn outsize graph canvas
 renderBarPlot <- function(data, title, names, x_label, y_label, colors, x_limit, y_limit) {
 	# debug data
-	message('BarPlot. data:')
-	message(data)
+	message('------------------')
+	message('BarPlot title:', title, '\ndata:\n', data, "\ndatatype:\n", typeof(data))
+	message('------------------')
 	return( renderPlot(barplot(
 			data,
 			main=title,
@@ -152,18 +154,26 @@ shinyServer(function(input, output) {
 	# ALTERNATIVE 2
 	#----------------------------------------
 	observeEvent(input$updateBtn2, {
-		Alt2        <- cbind(c(input$FishBiomass2, input$RiverRec2, input$Reservoir2, input$ProjectCost2, input$Safety2, input$NumProperties2, input$HydroCapacity2))
-		Alt2_Table      <- as.matrix(data.frame(Alt2))
+		# get decision inputs
+		Alt2 <- c(input$FishBiomass2, input$RiverRec2, input$Reservoir2, input$ProjectCost2, input$Safety2, input$NumProperties2, input$HydroCapacity2)
+
+		# create table matrix 1x5
+		Alt2_Table <- as.matrix(data.frame(Alt2))
 		row.names(Alt2_Table) <- variable_names
-		colnames(Alt2_Table)  <- "Raw Score"
-		t_Alt2_Table    <-t(Alt2_Table)
-		Alt2_Bar        <-barplot(t_Alt2_Table, main="Raw Scores Alternative 1",
-								  xlab=c("Fish Biomass", "River Recreation", "Reservoir Storage", "One-Time Project Costs", "Safety", "Number of Properties Impacted", "Hydropower Capacity"),
-								  col=colors)
-		Alt2_Results <- list(Alt2_Table, Alt2_Bar)
+		colnames(Alt2_Table) <- "Raw Score"
+
 		# results
-		output$SummTable2      <- renderTable(Alt2_Results[1])
-		output$SummPlot2       <- renderPlot(Alt2_Results[2])
+		output$SummTable2 <- renderTable(Alt2_Table, rownames=enable_rownames)
+		output$SummPlot2 <- renderBarPlot(
+								Alt2, # data
+								"Raw Scores of Alternative 2", # title
+								variable_names, # x_labels
+								"Topic", # x axis label
+								"Score", # y axis label
+								colors, # colors
+								NULL, # x value limit
+								score_range # y value limit (1-5 value range)
+							)
 	}) #end observe event
 
 
@@ -171,18 +181,26 @@ shinyServer(function(input, output) {
 	# ALTERNATIVE 3
 	#----------------------------------------
 	observeEvent(input$updateBtn3, {
-		Alt3        <- cbind(c(input$FishBiomass3, input$RiverRec3, input$Reservoir3, input$ProjectCost3, input$Safety3, input$NumProperties3, input$HydroCapacity3))
-		Alt3_Table      <- as.matrix(data.frame(Alt3))
-		row.names(Alt3_Table) <- c("Fish Biomass", "River Recreation", "Reservoir Storage", "One-Time Project Costs", "Safety", "Number of Properties Impacted", "Hydropower Capacity")
-		colnames(Alt3_Table)  <- "Raw Score"
-		t_Alt3_Table    <-t(Alt3_Table)
-		Alt3_Bar        <-barplot(t_Alt3_Table, main="Raw Scores Alternative 1",
-								  xlab=c("Fish Biomass", "River Recreation", "Reservoir Storage", "One-Time Project Costs", "Safety", "Number of Properties Impacted", "Hydropower Capacity"),
-								col=colors)
-		Alt3_Results <- list(Alt3_Table, Alt3_Bar)
+		# get decision inputs
+		Alt3 <- c(input$FishBiomass3, input$RiverRec3, input$Reservoir3, input$ProjectCost3, input$Safety3, input$NumProperties3, input$HydroCapacity3)
+
+		# create table matrix 1x5
+		Alt3_Table <- as.matrix(data.frame(Alt3))
+		row.names(Alt3_Table) <- variable_names
+		colnames(Alt3_Table) <- "Raw Score"
+
 		# results
-		output$SummTable3      <- renderTable(Alt3_Results[1])
-		output$SummPlot3       <- renderPlot(Alt3_Results[2])
+		output$SummTable3 <- renderTable(Alt3_Table, rownames=enable_rownames)
+		output$SummPlot3 <- renderBarPlot(
+								Alt3, # data
+								"Raw Scores of Alternative 3", # title
+								variable_names, # x_labels
+								"Topic", # x axis label
+								"Score", # y axis label
+								colors, # colors
+								NULL, # x value limit
+								score_range # y value limit (1-5 value range)
+							)
 	}) #end observe event
 
 
@@ -190,18 +208,26 @@ shinyServer(function(input, output) {
 	# ALTERNATIVE 4
 	#----------------------------------------
 	observeEvent(input$updateBtn4, {
-		Alt4        <- cbind(c(input$FishBiomass4, input$RiverRec4, input$Reservoir4, input$ProjectCost4, input$Safety4, input$NumProperties4, input$HydroCapacity4))
-		Alt4_Table      <- as.matrix(data.frame(Alt4))
-		row.names(Alt4_Table) <- c("Fish Biomass", "River Recreation", "Reservoir Storage", "One-Time Project Costs", "Safety", "Number of Properties Impacted", "Hydropower Capacity")
-		colnames(Alt4_Table)  <- "Raw Score"
-		t_Alt4_Table    <-t(Alt4_Table)
-		Alt4_Bar        <-barplot(t_Alt4_Table, main="Raw Scores Alternative 1",
-								  xlab=c("Fish Biomass", "River Recreation", "Reservoir Storage", "One-Time Project Costs", "Safety", "Number of Properties Impacted", "Hydropower Capacity"),
-								col=colors)
-		Alt4_Results <- list(Alt4_Table, Alt4_Bar)
+		# get decision inputs
+		Alt4 <- cbind(c(input$FishBiomass4, input$RiverRec4, input$Reservoir4, input$ProjectCost4, input$Safety4, input$NumProperties4, input$HydroCapacity4))
+
+		# create table matrix 1x5
+		Alt4_Table <- as.matrix(data.frame(Alt4))
+		row.names(Alt4_Table) <- variable_names
+		colnames(Alt4_Table) <- "Raw Score"
+
 		# results
-		output$SummTable4      <- renderTable(Alt4_Results[1])
-		output$SummPlot4       <- renderPlot(Alt4_Results[2])
+		output$SummTable4 <- renderTable(Alt4_Table, rownames=enable_rownames)
+		output$SummPlot4 <- renderBarPlot(
+								Alt4, # data
+								"Raw Scores of Alternative 4", # title
+								variable_names, # x_labels
+								"Topic", # x axis label
+								"Score", # y axis label
+								colors, # colors
+								NULL, # x value limit
+								score_range # y value limit (1-5 value range)
+							)
 	}) #end observe event
 
 
@@ -209,18 +235,26 @@ shinyServer(function(input, output) {
 	# ALTERNATIVE 5
 	#----------------------------------------
 	observeEvent(input$updateBtn5, {
-		Alt5        <- cbind(c(input$FishBiomass5, input$RiverRec5, input$Reservoir5, input$ProjectCost5, input$Safety5, input$NumProperties5, input$HydroCapacity5))
-		Alt5_Table      <- as.matrix(data.frame(Alt5))
-		row.names(Alt5_Table) <- c("Fish Biomass", "River Recreation", "Reservoir Storage", "One-Time Project Costs", "Safety", "Number of Properties Impacted", "Hydropower Capacity")
-		colnames(Alt5_Table)  <- "Raw Score"
-		t_Alt5_Table    <-t(Alt5_Table)
-		Alt5_Bar        <-barplot(t_Alt5_Table, main="Raw Scores Alternative 1",
-								  xlab=c("Fish Biomass", "River Recreation", "Reservoir Storage", "One-Time Project Costs", "Safety", "Number of Properties Impacted", "Hydropower Capacity"),
-								col=colors)
-		Alt5_Results <- list(Alt5_Table, Alt5_Bar)
+		# get decision inputs
+		Alt5 <- cbind(c(input$FishBiomass5, input$RiverRec5, input$Reservoir5, input$ProjectCost5, input$Safety5, input$NumProperties5, input$HydroCapacity5))
+
+		# create table matrix 1x5
+		Alt5_Table <- as.matrix(data.frame(Alt5))
+		row.names(Alt5_Table) <- variable_names
+		colnames(Alt5_Table) <- "Raw Score"
+
 		# results
-		output$SummTable5      <- renderTable(Alt5_Results[1])
-		output$SummPlot5       <- renderPlot(Alt5_Results[2])
+		output$SummTable5 <- renderTable(Alt5_Table, rownames=enable_rownames)
+		output$SummPlot5 <- renderBarPlot(
+								Alt5, # data
+								"Raw Scores of Alternative 5", # title
+								variable_names, # x_labels
+								"Topic", # x axis label
+								"Score", # y axis label
+								colors, # colors
+								NULL, # x value limit
+								score_range # y value limit (1-5 value range)
+							)
 	}) #end observe event
 
 
@@ -228,18 +262,26 @@ shinyServer(function(input, output) {
 	# ALTERNATIVE 6
 	#----------------------------------------
 	observeEvent(input$updateBtn6, {
-		Alt6        <- cbind(c(input$FishBiomass6, input$RiverRec6, input$Reservoir6, input$ProjectCost6, input$Safety6, input$NumProperties6, input$HydroCapacity6))
-		Alt6_Table      <- as.matrix(data.frame(Alt6))
-		row.names(Alt6_Table) <- c("Fish Biomass", "River Recreation", "Reservoir Storage", "One-Time Project Costs", "Safety", "Number of Properties Impacted", "Hydropower Capacity")
-		colnames(Alt6_Table)  <- "Raw Score"
-		t_Alt6_Table    <-t(Alt6_Table)
-		Alt6_Bar        <-barplot(t_Alt6_Table, main="Raw Scores Alternative 1",
-								  xlab=c("Fish Biomass", "River Recreation", "Reservoir Storage", "One-Time Project Costs", "Safety", "Number of Properties Impacted", "Hydropower Capacity"),
-								col=colors)
-		Alt6_Results <- list(Alt6_Table, Alt2_Bar)
+		# get decision inputs
+		Alt6 <- cbind(c(input$FishBiomass6, input$RiverRec6, input$Reservoir6, input$ProjectCost6, input$Safety6, input$NumProperties6, input$HydroCapacity6))
+
+		# create table matrix 1x6
+		Alt6_Table <- as.matrix(data.frame(Alt6))
+		row.names(Alt6_Table) <- variable_names
+		colnames(Alt6_Table) <- "Raw Score"
+
 		# results
-		output$SummTable6      <- renderTable(Alt6_Results[1])
-		output$SummPlot6       <- renderPlot(Alt6_Results[2])
+		output$SummTable6 <- renderTable(Alt6_Table, rownames=enable_rownames)
+		output$SummPlot6 <- renderBarPlot(
+								Alt6, # data
+								"Raw Scores of Alternative 6", # title
+								variable_names, # x_labels
+								"Topic", # x axis label
+								"Score", # y axis label
+								colors, # colors
+								NULL, # x value limit
+								score_range # y value limit (1-5 value range)
+							)
 	}) #end observe event
 
 
@@ -277,8 +319,17 @@ shinyServer(function(input, output) {
 		message('Results Done')
 		message(WSMResults[1])
 		message(WSMResults[2])
-		output$WSMTable <- renderTable(WSMResults[1])
-		output$WSMPlot  <- renderPlot({WSMResults[2]})
+		output$WSMTable <- renderTable(WSMResults[1], rownames=enable_rownames)
+		output$WSMPlot <- renderBarPlot(
+								WSMResults[2], # data
+								"WSM Ranked Alternatives", # title
+								wsm_bar_plot_names, # x_labels
+								"Topic", # x axis label
+								"Score", # y axis label
+								colors, # colors
+								NULL, # x value limit
+								score_range # y value limit (1-5 value range)
+							)
 	})   # end generate button event
 
 
