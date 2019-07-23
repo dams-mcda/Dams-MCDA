9f30a145792dbc76145bba035ebb6d1e748e6d29
ELBFOX <ELBFOX@gmail.com>
2019-07-23 17:27:08 -0400

    updated 3d matrix to array format, working on normalization operation

--- src/dams_mcda/WSM.R
+++ src/dams_mcda/WSM.R
@@ -125,6 +125,7 @@ WSM <- function(CritImportance, RawCriteriaMatrix, DamsData){
 	                DamsData$Culture_Remove, DamsData$History_Remove, DamsData$Community_Remove, DamsData$Aesthetics_Remove, 
 	                DamsData$Health_Remove, DamsData$Justice_Remove)
 	
+	#This naming technique is wildly redundant and doesn't appear to work. 
 	names(KeepMaintain) <- c(
 	  "FishBiomass",
 	  "RiverRec",
@@ -210,21 +211,22 @@ WSM <- function(CritImportance, RawCriteriaMatrix, DamsData){
 	  "Justice"
 	)
 	  
-	AlternativeLevels <- cbind.hyperframe(KeepMaintain, Improve_Hydro, Improve_Fish, FishANDHydro, Remove)
+	#This abind creates our 3D matrix
+	DamsDataMatrix <- abind(KeepMaintain, Improve_Hydro, Improve_Fish, FishANDHydro, Remove, along = 3, force.array=TRUE)
 	
-	
+
 	
 	#--------NORMALIZATION-------------------
 	# iterate each criteria for min,max
 	WSMMaxVector <- list("list", matrix_cols)
 	for ( index in 1:matrix_cols ){
-		WSMMaxVector[[index]] <- max(DamsDataMatrix[,index], na.rm=FALSE)
+		WSMMaxVector[[index]] <- max(DamsDataMatrix[,index,], na.rm=FALSE)#specified index level 7/23/2019
 	}
 	WSMMaxVector <- unlist(WSMMaxVector)
 
 	WSMMinVector <- list("list", matrix_cols)
 	for ( index in 1:matrix_cols ){
-		WSMMinVector[[index]] <- min(DamsDataMatrix[,index], na.rm=FALSE)
+		WSMMinVector[[index]] <- min(DamsDataMatrix[,index,], na.rm=FALSE)#specified index level 7/23/2019
 	}
 	WSMMinVector <- unlist(WSMMinVector)
 
@@ -240,19 +242,20 @@ WSM <- function(CritImportance, RawCriteriaMatrix, DamsData){
 	#----------------------------------------
 	WSMScoreMatrix <- data.frame(matrix(data=NA, nrow = matrix_rows, ncol = matrix_cols, nlevels = matrix_levs))
 	# array of rows that use alternative method
-	alt_method_columns <- c(4, 6) ####THIS LIKELY NEEDS TO CHANGE
+	alt_method_columns <- c(8, 14, 5) 
 
-	message('length of critImportance', length(CritImportance)) ###THIS ALSO LIKELY NEEDS TO CHANGE
+	message('length of critImportance', length(CritImportance)) 
 	# make normalized values of each value in matrix ---> check DOES THIS NEED TO BE 3-D array, too?
 	for (k in 1:matrix_cols){
 		for (n in 1:matrix_rows){
-			x <- RawCriteriaMatrix[n,k]
+		  for (p in 1:matrix_levs){#added levels 7/23/2019
+			x <- DamsDataMatrix[n,k,p]
 			min_x <- WSMMinVector[k]
 			max_x <- WSMMaxVector[k]
 			crit_imp <- CritImportance[n,k]
 
 
-			WSMScoreMatrix[n,k] <- tryCatch({
+			WSMScoreMatrix[n,k,p] <- tryCatch({
 				if (k %in% alt_method_columns){
 					# alternative method
 					# maximize normalization
@@ -269,8 +272,9 @@ WSM <- function(CritImportance, RawCriteriaMatrix, DamsData){
 				(NA)
 			})
 
-		} #End alternative (rows) for loop.
-		message('Raw column ', RawCriteriaMatrix[k])
+		  } #End alternative (levels) for loop.
+		} #End dam (rows) for loop.
+		message('Raw column ', DamsDataMatrix[k])
 		message('WSM column ', WSMScoreMatrix[k])
 	} #End criteria (columns) for loop.
 
